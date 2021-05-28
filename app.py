f463401eb3cae1661a8f1511b4463ae4063140e4
Your Name <you@example.com>
2021-05-28 11:09:32 +0900

    articles.gtml

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from data import Articles
 
-app=Flask(__name__)
-app.debug-True
+app = Flask(__name__)
+app.debug = True
 
-@app.route('/',methodes=['GET', 'POST'])
+@app.route('/', methods=['GET' , 'POST'])
+
 def hello_world():
-    return 'Hello World!'
+    return render_template('Home.html', name="정경수")
 
+@app.route('/about',methods=['GET','POST'])
+def about():
+    return render_template('about.html')
 
-if __name__ =='__main__':
+@app.route('/articles',methods=['GET','POST'])
+def articles():
+    articles=Articles()
+    return render_template('articles.html', articles = articles)
+
+    
+if __name__ == '__main__':
     app.run(port=5000)
